b0344c408d391a1053c2e06766e7fdb01f4d02f0
WillSiuta <willsiuta@gmail.com>
2023-01-12 00:06:38 -0500

    Adding PyPoll.py.file.

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
